ec1c28130346b8f526f82ec35f99c6834854b1ec
Anthony Lloyd <anthony@lloydhq.com>
2020-09-04 22:30:42 +0100

    to 2020.2

--- Tests/GeneralTests.fs
+++ Tests/GeneralTests.fs
@@ -8,9 +8,9 @@ let all =
             let v = MKL.General.get_version()
             Check.equal v.MajorVersion 2020
             Check.equal v.MinorVersion 0
-            Check.equal v.UpdateVersion 1
+            Check.equal v.UpdateVersion 2
             Check.equal v.ProductStatus "Product"
-            Check.equal v.Build "20200208"
+            Check.equal v.Build "20200624"
             Check.info "%s" v.Processor
             Check.info "%s" v.Platform
         }
