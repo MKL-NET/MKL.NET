96d6538411e9caaebc1ea7ead68f08fddc38586d
Anthony Lloyd <anthony@lloydhq.com>
2020-09-08 19:38:00 +0100

    new IntPtr

--- Tests/VslTests.fs
+++ Tests/VslTests.fs
@@ -7,7 +7,7 @@ let all =
     test "Vls" {
 
         test "stream" {
-            let mutable stream = IntPtr.Zero
+            let mutable stream = IntPtr()
             Vsl.NewStream(&stream, 3145728, 77u) |> Check.equal 0
             let r = Array.zeroCreate 3
             Vsl.dRngGaussian(2, stream, 5, r, 0.0, 1.0) |> Check.equal 0
