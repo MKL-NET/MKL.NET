0aea446d48a96e120bd7cf89a1334038ec4aa0f2
Anthony Lloyd <anthony@lloydhq.com>
2020-11-01 23:10:30 +0000

    matrix model

--- MKL.NET.Matrix/Matrix.cs
+++ MKL.NET.Matrix/Matrix.cs
@@ -18,6 +18,8 @@ namespace MKLNET
             Vml.Sqrt(m.Length, m.A, 0, 1, r.A, 0, 1);
             return r;
         }
+        public static matrixS Sqrt(matrixS m) => Sqrt(m.Matrix) * Math.Sqrt(m.Scale);
+        public static matrixT Sqrt(matrixT m) => new matrixT(Sqrt(m.Matrix));
     }
     public class matrix : IDisposable
     {
@@ -55,6 +57,9 @@ namespace MKLNET
             Vml.Sub(a.Length, a.A, 0, 1, b.A, 0, 1, r.A, 0, 1);
             return r;
         }
+        public matrixT T => new matrixT(this);
+        public static matrixS operator *(matrix a, double s) => new matrixS(a, s);
+        public static matrixS operator *(double s, matrix a) => new matrixS(a, s);
         public static matrix operator *(matrix a, matrix b)
         {
             var r = new matrix(a.Rows, b.Cols);
@@ -63,45 +68,67 @@ namespace MKLNET
         }
         public static matrix operator *(matrix a, matrixT b)
         {
-            var r = new matrix(a.Rows, b.Cols);
-            Blas.gemm(Layout.ColMajor, Trans.No, Trans.Yes, a.Rows, b.Cols, a.Cols, 1.0, a.A, a.Rows, b.A, b.Rows, 0.0, r.A, a.Rows);
+            var bT = b.Matrix;
+            var r = new matrix(a.Rows, bT.Cols);
+            Blas.gemm(Layout.ColMajor, Trans.No, Trans.Yes, a.Rows, bT.Cols, a.Cols, 1.0, a.A, a.Rows, bT.A, bT.Rows, 0.0, r.A, a.Rows);
             return r;
         }
-        public matrixT T => new matrixT(Cols, Rows, A);
-    }
-
-    public class matrixT
-    {
-        public readonly int Rows;
-        public readonly int Cols;
-        public readonly double[] A;
-        internal matrixT(int rows, int cols, double[] a)
+        public static matrix operator *(matrix a, matrixS bS)
         {
-            Rows = rows;
-            Cols = cols;
-            A = a;
+            var b = bS.Matrix;
+            var r = new matrix(a.Rows, b.Cols);
+            Blas.gemm(Layout.ColMajor, Trans.No, Trans.No, a.Rows, b.Cols, a.Cols, bS.Scale, a.A, a.Rows, b.A, b.Rows, 0.0, r.A, a.Rows);
+            return r;
         }
-        public double this[int row, int col] => A[row * Cols + col];
     }
 
-    public class matrixF
+    public struct matrixT
     {
-        readonly int Rows;
-        readonly int Cols;
-        readonly float[] A;
-        internal matrixF(int rows, int cols, float[] a)
+        public readonly matrix Matrix;
+        internal matrixT(matrix m)
         {
-            Rows = rows;
-            Cols = cols;
-            A = a;
+            Matrix = m;
+        }
+    }
+
+    public struct matrixS
+    {
+        public readonly matrix Matrix;
+        public readonly double Scale;
+        internal matrixS(matrix m, double s)
+        {
+            Matrix = m;
+            Scale = s;
+        }
+        public static implicit operator matrix(matrixS d) => d.Matrix;
+        public static matrix operator *(matrixS aS, matrix b)
+        {
+            var a = aS.Matrix;
+            var r = new matrix(a.Rows, b.Cols);
+            Blas.gemm(Layout.ColMajor, Trans.No, Trans.No, a.Rows, b.Cols, a.Cols, aS.Scale, a.A, a.Rows, b.A, b.Rows, 0.0, r.A, a.Rows);
+            return r;
+        }
+        public static matrix operator *(matrixS aS, matrixT b)
+        {
+            var a = aS.Matrix;
+            var bT = b.Matrix;
+            var r = new matrix(a.Rows, bT.Cols);
+            Blas.gemm(Layout.ColMajor, Trans.No, Trans.Yes, a.Rows, bT.Cols, a.Cols, aS.Scale, a.A, a.Rows, bT.A, bT.Rows, 0.0, r.A, a.Rows);
+            return r;
+        }
+        public static matrix operator *(matrixS aS, matrixS bS)
+        {
+            var a = aS.Matrix;
+            var b = bS.Matrix;
+            var r = new matrix(a.Rows, b.Cols);
+            Blas.gemm(Layout.ColMajor, Trans.No, Trans.No, a.Rows, b.Cols, a.Cols, aS.Scale * bS.Scale, a.A, a.Rows, b.A, b.Rows, 0.0, r.A, a.Rows);
+            return r;
         }
     }
 
     public static class Test
     {
-        public static matrix Test1(matrix a, matrix b)
-        {
-            return a + b;
-        }
+        public static matrix Test1(matrix a, matrix b) => 2.0 * a * b;
+        public static matrixS Test2(matrix a) => Matrix.Sqrt(2.0 * a);
     }
 }
