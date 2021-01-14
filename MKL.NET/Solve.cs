c82f7e5c36397b9f71b08d6878973cd6b2398e1c
Anthony Lloyd <anthony@lloydhq.com>
2021-01-14 17:10:56 +0000

    solve bound

--- MKL.NET/Solve.cs
+++ MKL.NET/Solve.cs
@@ -22,6 +22,12 @@ namespace MKLNET
         [DllImport(DLL, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
         static extern RciStatus dtrnlsp_delete(IntPtr* handle);
         [DllImport(DLL, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
+        static extern RciStatus dtrnlspbc_init(IntPtr* handle, int* n, int* m, double* x, double* lower, double* upper, double* eps, int* iter1, int* iter2, double* rs);
+        [DllImport(DLL, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
+        static extern RciStatus dtrnlspbc_solve(IntPtr* handle, double* fvec, double* fjac, int* RCI_Request);
+        [DllImport(DLL, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
+        static extern RciStatus dtrnlspbc_delete(IntPtr* handle);
+        [DllImport(DLL, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
         static extern RciStatus djacobi_init(IntPtr* handle, int* n, int* m, double* x, double* fjac, double* eps);
         [DllImport(DLL, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
         static extern RciStatus djacobi_solve(IntPtr* handle, double* f1, double* f2, int* RCI_Request);
@@ -104,18 +110,77 @@ namespace MKLNET
             }
         }
 
-        ///* Function prototypes */
-        //extern MKL_INT dtrnlsp_init(_TRNSP_HANDLE_t*, const MKL_INT*, const MKL_INT*, const double*, const double*, const MKL_INT*, const MKL_INT*, const double*);
-        //extern MKL_INT dtrnlsp_check(_TRNSP_HANDLE_t*, const MKL_INT*, const MKL_INT*, const double*, const double*, const double*, MKL_INT*);
-        //extern MKL_INT dtrnlsp_solve(_TRNSP_HANDLE_t*, double*, double*, MKL_INT*);
-        //extern MKL_INT dtrnlsp_get(_TRNSP_HANDLE_t*, MKL_INT*, MKL_INT*, double*, double*);
-        //extern MKL_INT dtrnlsp_delete(_TRNSP_HANDLE_t*);
+        public static RciStatus NonLinearLeastSquares(Action<double[], double[]> F, double[] x, double[] lower, double[] upper, double[] Fx, double[] eps, int iter1, int iter2, double rs)
+        {
+            int n = x.Length;
+            int m = Fx.Length;
+            double[] jac = new double[n * m];
+            double[] f1 = new double[m];
+            double[] f2 = new double[m];
+            fixed (double* xp = &x[0], lowerp = &lower[0], upperp = &upper[0], epsp = &eps[0], Fxp = &Fx[0], jacp = &jac[0], f1p = &f1[0], f2p = &f2[0])
+            {
+                IntPtr handle;
+                int request;
+                var status = dtrnlspbc_init(&handle, &n, &m, xp, lowerp, upperp, epsp, &iter1, &iter1, &rs);
+                while (status == RciStatus.SUCCESS && (status = dtrnlspbc_solve(&handle, Fxp, jacp, &request)) == RciStatus.SUCCESS)
+                {
+                    if (request == CALCULATE_FUNCTION)
+                    {
+                        F(x, Fx);
+                    }
+                    else if (request == CALCULATE_JACOBIAN)
+                    {
+                        IntPtr jacHandle;
+                        int jacRequest;
+                        status = djacobi_init(&jacHandle, &n, &m, xp, jacp, epsp);
+                        if (status == RciStatus.SUCCESS)
+                            while ((status = djacobi_solve(&jacHandle, f1p, f2p, &jacRequest)) == RciStatus.SUCCESS && jacRequest != 0)
+                                F(x, jacRequest == 1 ? f1 : f2);
+                        djacobi_delete(&jacHandle);
+                    }
+                    else if (request < 0)
+                    {
+                        status = (RciStatus)request;
+                        break;
+                    }
+                }
+                dtrnlspbc_delete(&handle);
+                return status;
+            }
+        }
 
-        //extern MKL_INT dtrnlspbc_init(_TRNSPBC_HANDLE_t*, const MKL_INT*, const MKL_INT*, const double*, const double*, const double*, const double*, const MKL_INT*, const MKL_INT*, const double*);
-        //extern MKL_INT dtrnlspbc_check(_TRNSPBC_HANDLE_t*, const MKL_INT*, const MKL_INT*, const double*, const double*, const double*, const double*, const double*, MKL_INT*);
-        //extern MKL_INT dtrnlspbc_solve(_TRNSPBC_HANDLE_t*, double*, double*, MKL_INT*);
-        //extern MKL_INT dtrnlspbc_get(_TRNSPBC_HANDLE_t*, MKL_INT*, MKL_INT*, double*, double*);
-        //extern MKL_INT dtrnlspbc_delete(_TRNSPBC_HANDLE_t*);
+        public static RciStatus NonLinearLeastSquares(djacobi_function F, double[] x, double[] lower, double[] upper, double[] Fx, double[] eps, int iter1, int iter2, double rs)
+        {
+            int n = x.Length;
+            int m = Fx.Length;
+            double[] jac = new double[n * m];
+            double[] f1 = new double[m];
+            double[] f2 = new double[m];
+            fixed (double* xp = &x[0], lowerp = &lower[0], upperp = &upper[0], epsp = &eps[0], Fxp = &Fx[0], jacp = &jac[0], f1p = &f1[0], f2p = &f2[0])
+            {
+                IntPtr handle;
+                int request;
+                var status = dtrnlspbc_init(&handle, &n, &m, xp, lowerp, upperp, epsp, &iter1, &iter1, &rs);
+                while (status == RciStatus.SUCCESS && (status = dtrnlspbc_solve(&handle, Fxp, jacp, &request)) == RciStatus.SUCCESS)
+                {
+                    if (request == CALCULATE_FUNCTION)
+                    {
+                        F(&m, &n, xp, Fxp);
+                    }
+                    else if (request == CALCULATE_JACOBIAN)
+                    {
+                        status = djacobi(F, &n, &m, jacp, xp, epsp);
+                    }
+                    else if (request < 0)
+                    {
+                        status = (RciStatus)request;
+                        break;
+                    }
+                }
+                dtrnlspbc_delete(&handle);
+                return status;
+            }
+        }
     }
 
     public enum RciStatus
--- Tests/SolveTests.fs
+++ Tests/SolveTests.fs
@@ -50,4 +50,50 @@ let all = test "rci" {
         Check.info "Fx   : %A" Fx
         Check.info "Calls: %i" calls
     }
+
+    test "quadratic_action_bound" {
+        let mutable calls = 0
+        let F (x:double[]) (Fx:double[]) =
+            Interlocked.Increment(&calls) |> ignore
+            let a, b = x.[0], x.[1]
+            let x, y = 0.0, -1.0
+            Fx.[0] <- y - (x-a) * (x-b)
+            let x,y = 1.0, 0.0
+            Fx.[1] <- y - (x-a) * (x-b)
+            let x,y = -1.0, 0.0
+            Fx.[2] <- y - (x-a) * (x-b)
+        let eps = Array.create 6 0.00001
+        let x = [|-5.0; 8.0|]
+        let lower = [|-2.0; -2.0|]
+        let upper = [|2.0; 2.0|]
+        let Fx = Array.zeroCreate 3
+        let result = Solve.NonLinearLeastSquares(Action<_,_> F, x, lower, upper, Fx, eps, 1000, 100, 0.0)
+        Check.equal RciStatus.F_NORM_2_LESS_THAN_EPS1 result
+        Check.info "x    : %A" x
+        Check.info "Fx   : %A" Fx
+        Check.info "Calls: %i" calls
+    }
+
+    test "quadratic_callback_bound" {
+        let mutable calls = 0
+        let F m n (x:nativeptr<double>) (Fx:nativeptr<double>) =
+            Interlocked.Increment(&calls) |> ignore
+            let a, b = NativePtr.get x 0, NativePtr.get x 1
+            let x, y = 0.0, -1.0
+            NativePtr.set Fx 0  (y - (x-a) * (x-b))
+            let x,y = 1.0, 0.0
+            NativePtr.set Fx 1  (y - (x-a) * (x-b))
+            let x,y = -1.0, 0.0
+            NativePtr.set Fx 2  (y - (x-a) * (x-b))
+        let eps = Array.create 6 0.00001
+        let x = [|-5.0; 8.0|]
+        let lower = [|-2.0; -2.0|]
+        let upper = [|2.0; 2.0|]
+        let Fx = Array.zeroCreate 3
+        let result = Solve.NonLinearLeastSquares(Solve.djacobi_function F, x, lower, upper, Fx, eps, 1000, 100, 0.0)
+        Check.equal RciStatus.F_NORM_2_LESS_THAN_EPS1 result
+        Check.info "x    : %A" x
+        Check.info "Fx   : %A" Fx
+        Check.info "Calls: %i" calls
+    }
 }
