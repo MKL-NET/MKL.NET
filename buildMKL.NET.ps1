c1002ae2d747c5690c9e4bdf55ecdd723acf5af3
Anthony Lloyd <anthony@lloydhq.com>
2021-06-17 17:47:16 +0100

    Reference Assembly rename and size

--- buildMKL.NET.ps1
+++ buildMKL.NET.ps1
@@ -3,4 +3,4 @@ dotnet build .\MKL.NET\MKL.NET.csproj -c Release -r win-x86
 dotnet build .\MKL.NET\MKL.NET.csproj -c Release -r linux-x64 -p:OS=LINUX
 dotnet build .\MKL.NET\MKL.NET.csproj -c Release -r linux-x86 -p:OS=LINUX
 dotnet build .\MKL.NET\MKL.NET.csproj -c Release -r osx-x64 -p:OS=OSX
-dotnet pack MKL.NET -c Release -o packages
+dotnet pack MKL.NET -c Release -o packages -p:OS=REF
