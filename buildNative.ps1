19be314fb4e76f3263be302e273f0b2691ccd477
Anthony Lloyd <anthony@lloydhq.com>
2021-07-01 09:47:20 +0100

    mkl native lib update for win

--- buildNative.ps1
+++ buildNative.ps1
@@ -20,24 +20,24 @@ try
     if(Test-Path runtimes) { Remove-Item runtimes -Force -Recurse }
     New-Item runtimes -ItemType Directory | Out-Null
 
-    GetPackage intelmkl.redist.win-x64 2021.2.0.296
+    GetPackage intelmkl.redist.win-x64 2021.3.0.524
     New-Item runtimes/win-x64/native -ItemType Directory | Out-Null
     Copy-Item packages/intelmkl.redist.win-x64/runtimes/win-x64/native/*.dll runtimes/win-x64/native
     Remove-Item packages/intelmkl.redist.win-x64 -Force -Recurse
     Move-Item runtimes/win-x64/native/mkl_rt.1.dll runtimes/win-x64/native/mkl_rt.dll
 
-    GetPackage intelmkl.redist.win-x86 2021.2.0.296
+    GetPackage intelmkl.redist.win-x86 2021.3.0.524
     New-Item runtimes/win-x86/native -ItemType Directory | Out-Null
     Copy-Item packages/intelmkl.redist.win-x86/runtimes/win-x86/native/*.dll runtimes/win-x86/native
     Remove-Item packages/intelmkl.redist.win-x86 -Force -Recurse
     Move-Item runtimes/win-x86/native/mkl_rt.1.dll runtimes/win-x86/native/mkl_rt.dll
     
-    GetPackage intelopenmp.redist.win 2021.1.2.265
+    GetPackage intelopenmp.redist.win 2021.3.0.3372
     Copy-Item packages/intelopenmp.redist.win/runtimes/win-x64/native/*.dll runtimes/win-x64/native
     Copy-Item packages/intelopenmp.redist.win/runtimes/win-x86/native/*.dll runtimes/win-x86/native
     Remove-Item packages/intelopenmp.redist.win -Force -Recurse
     
-    GetPackage inteltbb.redist.win 2021.2.0.371
+    GetPackage inteltbb.redist.win 2021.3.0.511
     Copy-Item packages/inteltbb.redist.win/runtimes/win-x64/native/*.dll runtimes/win-x64/native
     Copy-Item packages/inteltbb.redist.win/runtimes/win-x86/native/*.dll runtimes/win-x86/native
     Remove-Item packages/inteltbb.redist.win -Force -Recurse
@@ -52,12 +52,12 @@ try
     Copy-Item packages/intelmkl.devel.linux-x86/lib/native/linux-x86/*.so runtimes/linux-x86/native
     Remove-Item packages/intelmkl.devel.linux-x86 -Force -Recurse
 
-    GetPackage intelopenmp.devel.linux 2021.2.0.610
+    GetPackage intelopenmp.devel.linux 2021.3.0.3350
     Copy-Item packages/intelopenmp.devel.linux/lib/native/linux-x64/*.so runtimes/linux-x64/native
     Copy-Item packages/intelopenmp.devel.linux/lib/native/linux-x86/*.so runtimes/linux-x86/native
     Remove-Item packages/intelopenmp.devel.linux -Force -Recurse
 
-    GetPackage inteltbb.devel.linux 2021.2.0.357
+    GetPackage inteltbb.devel.linux 2021.3.0.511
     Copy-Item packages/inteltbb.devel.linux/runtimes/linux-x64/native/*.so runtimes/linux-x64/native
     Copy-Item packages/inteltbb.devel.linux/runtimes/linux-x86/native/*.so runtimes/linux-x86/native
     Remove-Item packages/inteltbb.devel.linux -Force -Recurse
@@ -75,18 +75,20 @@ try
     Remove-Item runtimes/osx-x64/native/libmkl_tbb_thread.dylib
     Move-Item runtimes/osx-x64/native/libmkl_rt.1.dylib runtimes/osx-x64/native/libmkl_rt.dylib
 
-    GetPackage intelopenmp.devel.osx 2021.2.0.564
+    GetPackage intelopenmp.devel.osx 2021.3.0.3375
     Copy-Item packages/intelopenmp.devel.osx/lib/native/osx-x64/*.dylib runtimes/osx-x64/native
     Remove-Item packages/intelopenmp.devel.osx -Force -Recurse
 
-    GetPackage inteltbb.devel.osx 2021.2.0.332
+    GetPackage inteltbb.devel.osx 2021.3.0.511
     Copy-Item packages/inteltbb.devel.osx/runtimes/osx-x64/native/*.dylib runtimes/osx-x64/native
     Remove-Item packages/inteltbb.devel.osx -Force -Recurse
 
-    dotnet pack MKL.NET.win-x64 -o packages -p:Version=2021.2.0.296
-    dotnet pack MKL.NET.win-x86 -o packages -p:Version=2021.2.0.296
+    dotnet pack MKL.NET.win-x64 -o packages -p:Version=2021.3.0.524
+    dotnet pack MKL.NET.win-x86 -o packages -p:Version=2021.3.0.524
     dotnet pack MKL.NET.linux-x64 -o packages -p:Version=2020.4.304
     dotnet pack MKL.NET.linux-x86 -o packages -p:Version=2021.2.0.296
     dotnet pack MKL.NET.osx-x64 -o packages -p:Version=2021.2.0.269
 }
 finally { Pop-Location }
+
+# dotnet nuget push .\packages\MKL.NET.win-x64.2021.3.0.524.nupkg -k $env:NUGET_KEY -s https://api.nuget.org/v3/index.json
