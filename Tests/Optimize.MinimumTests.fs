ccdf02bf70eb353a3ec37813926e136a4487f167
Anthony Lloyd <anthony@lloydhq.com>
2021-08-24 23:04:40 +0100

    optimize.minimum n dimension more tests

--- Tests/Optimize.MinimumTests.fs
+++ Tests/Optimize.MinimumTests.fs
@@ -91,7 +91,7 @@ let all =
             Optimize.Minimum_Bracketed(atol, rtol, f, a, f.Invoke(a), b, f.Invoke(b), c, f.Invoke(c), Double.PositiveInfinity, 0.0)
 
         test_solver "hybrid_bracketed_7" 1e-7 minimum_bracketed (Check.between 2721 2721)
-        test_solver "hybrid_bracketed_9" 1e-9 minimum_bracketed (Check.between 3217 3217)
+        test_solver "hybrid_bracketed_9" 1e-9 minimum_bracketed (Check.between 3216 3217)
         test_solver "hybrid_bracketed_11" 1e-11 minimum_bracketed (Check.between 4171 4179)
 
         let minimum (atol, rtol, f:Func<float,float>, _, b, _) = Optimize.Minimum(atol, rtol, f, b)
