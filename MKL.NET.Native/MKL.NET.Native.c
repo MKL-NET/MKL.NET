f41fe6bc1778d61ac6ec8df6f22a0298542d3fcf
Anthony Lloyd <anthony@lloydhq.com>
2021-09-11 10:19:17 +0100

    start MKL.NET.Native

---
+++ MKL.NET.Native/MKL.NET.Native.c
@@ -0,0 +1,12 @@
+﻿#include <stdlib.h>
+
+#ifdef _WIN32
+#  define EXPORTIT __declspec( dllexport )
+#else
+#  define EXPORTIT __attribute__((visibility("default")))
+#endif
+
+EXPORTIT void test(int i)
+{
+
+}
