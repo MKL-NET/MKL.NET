1de405fccd054e26884810871fbaa2c5509421fa
Anthony Lloyd <anthony@lloydhq.com>
2021-09-13 20:43:19 +0100

    try Native on windows

--- buildMKL.NET.ps1
+++ buildMKL.NET.ps1
@@ -1,6 +1,6 @@
-dotnet build .\MKL.NET\MKL.NET.csproj -c Release -r win-x64
-dotnet build .\MKL.NET\MKL.NET.csproj -c Release -r win-x86
-dotnet build .\MKL.NET\MKL.NET.csproj -c Release -r linux-x64 -p:MKLNET_OS=LINUX
-dotnet build .\MKL.NET\MKL.NET.csproj -c Release -r linux-x86 -p:MKLNET_OS=LINUX
-dotnet build .\MKL.NET\MKL.NET.csproj -c Release -r osx-x64 -p:MKLNET_OS=OSX
-dotnet pack MKL.NET -c Release -o packages -p:MKLNET_OS=REF
+dotnet build MKL.NET -c Release -p:MKLNET_RID=win-x64
+dotnet build MKL.NET -c Release -p:MKLNET_RID=win-x86
+dotnet build MKL.NET -c Release -p:MKLNET_RID=linux-x64
+dotnet build MKL.NET -c Release -p:MKLNET_RID=linux-x86
+dotnet build MKL.NET -c Release -p:MKLNET_RID=osx-x64
+dotnet pack MKL.NET -c Release -o packages -p:MKLNET_RID=ref
