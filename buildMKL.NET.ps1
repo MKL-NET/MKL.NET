8a513f17473bae993431edc989f2db048d39b170
Anthony Lloyd <anthony@lloydhq.com>
2021-09-14 14:27:18 +0100

    try Native on all

--- buildMKL.NET.ps1
+++ buildMKL.NET.ps1
@@ -1,6 +1,6 @@
-dotnet build MKL.NET -c Release -r win-x64
-dotnet build MKL.NET -c Release -r win-x86
-dotnet build MKL.NET -c Release -r linux-x64
-dotnet build MKL.NET -c Release -r linux-x86
-dotnet build MKL.NET -c Release -r osx-x64
-dotnet pack MKL.NET -c Release -o packages -p:ISREF=true
+dotnet build MKL.NET -c win-x64
+dotnet build MKL.NET -c win-x86
+dotnet build MKL.NET -c linux-x64
+dotnet build MKL.NET -c linux-x86
+dotnet build MKL.NET -c osx-x64
+dotnet pack MKL.NET -c ref -o packages
