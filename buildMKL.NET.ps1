e6c4ad993f9cbde0ed38a4d93329cc6651680a2c
Anthony Lloyd <anthony@lloydhq.com>
2021-09-14 17:24:51 +0100

    remove linux 86

--- buildMKL.NET.ps1
+++ buildMKL.NET.ps1
@@ -1,6 +1,5 @@
 dotnet build MKL.NET -c win-x64
 dotnet build MKL.NET -c win-x86
 dotnet build MKL.NET -c linux-x64
-dotnet build MKL.NET -c linux-x86
 dotnet build MKL.NET -c osx-x64
 dotnet pack MKL.NET -c ref -o packages
