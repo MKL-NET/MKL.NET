bf9aa61f6ac95bba164b2e17e677c2289099e486
Anthony Lloyd <anthony@lloydhq.com>
2021-09-15 15:40:03 +0100

    native devel

--- Scripts/Download-Devel.ps1
+++ Scripts/Download-Devel.ps1
@@ -13,3 +13,4 @@ if(Test-Path $dir) { Remove-Item $dir -Force -Recurse }
 if(!(Test-Path $dir)) { New-Item $dir }
 if($args.count -eq 0 -or $args[0] -eq 'win-x64') { GetPackage intelmkl.devel.win-x64 2021.3.0.524 }
 if($args.count -eq 0 -or $args[0] -eq 'win-x86') { GetPackage intelmkl.devel.win-x86 2021.3.0.524 }
+if($args.count -eq 0 -or $args[0] -eq 'linux-x64') { GetPackage intelmkl.devel.linux-x64 2021.3.0.524 }
