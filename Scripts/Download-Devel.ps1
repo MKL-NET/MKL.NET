3196b090ff12b098748bd8d665591953ed5d2093
Anthony Lloyd <anthony@lloydhq.com>
2021-09-17 13:52:21 +0100

    native dfti all

--- Scripts/Download-Devel.ps1
+++ Scripts/Download-Devel.ps1
@@ -12,6 +12,14 @@ function GetPackage([string]$name, [string]$version)
 if(Test-Path $dir) { Remove-Item $dir -Force -Recurse }
 if(!(Test-Path $dir)) { New-Item $dir -ItemType Directory | Out-Null }
 
-if($args.count -eq 0 -or $args[0] -eq 'win-x64' -or $args[0] -eq 'win-x86') { GetPackage intelopenmp.devel.win 2021.3.0.3372 }
+#if($args.count -eq 0 -or $args[0] -eq 'win-x64' -or $args[0] -eq 'win-x86') { GetPackage intelopenmp.devel.win 2021.3.0.3372 }
 if($args.count -eq 0 -or $args[0] -eq 'win-x64') { GetPackage intelmkl.devel.win-x64 2021.3.0.524 }
 if($args.count -eq 0 -or $args[0] -eq 'win-x86') { GetPackage intelmkl.devel.win-x86 2021.3.0.524 }
+if($args.count -eq 0 -or $args[0] -eq 'linux-x64') {
+    GetPackage intelmkl.devel.linux-x64 2020.4.304
+    #GetPackage intelopenmp.devel.linux 2021.3.0.3350
+}
+if($args.count -eq 0 -or $args[0] -eq 'osx-x64') {
+    GetPackage intelmkl.devel.osx-x64 2021.2.0.269
+    #GetPackage intelopenmp.devel.osx 2021.3.0.3375
+}
