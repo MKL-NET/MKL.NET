bc69a304b2c4f7bc8b7834a0af1e3ae3543209d7
Anthony Lloyd <anthony@lloydhq.com>
2021-10-12 22:10:18 +0100

    packaging fixup

--- Scripts/Package-MKL.NET.ps1
+++ Scripts/Package-MKL.NET.ps1
@@ -2,4 +2,8 @@ dotnet build MKL.NET -c Release -r win-x64
 dotnet build MKL.NET -c Release -r win-x86
 dotnet build MKL.NET -c Release -r linux-x64
 dotnet build MKL.NET -c Release -r osx-x64
-dotnet pack MKL.NET -c Release -r ref -o packages
+dotnet build MKL.NET -c Release -r osx-x86 # The ref assembly
+dotnet pack MKL.NET -c Release -o packages
+dotnet pack MKL.NET.Matrix -c Release -o packages -p:NORID=true
+dotnet pack MKL.NET.Optimization -c Release -o packages -p:NORID=true
+dotnet pack MKL.NET.Statistics -c Release -o packages -p:NORID=true
