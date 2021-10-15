e3fd53f3442233a2e8f0b0471c5b9aa2c7ed271c
Anthony Lloyd <anthony@lloydhq.com>
2021-10-15 19:43:27 +0100

    moment estimators with tests

--- Tests/Stats.EstimatorTests.fs
+++ Tests/Stats.EstimatorTests.fs
@@ -17,7 +17,7 @@ let quartile = test "quartile" {
         Check.greaterThanOrEqual qe.Q3 qe.Q4
     }
 
-    test "p2" {
+    test "vs_p2" {
         let! xs = Gen.Double.[-1000.0, 1000.0].Array.[5, 50]
         let expected = P2QuantileEstimator(0.5)
         let actual = QuartileEstimator()
@@ -83,7 +83,21 @@ let quartile = test "quartile" {
     }
 }
 
-let moment = test "moment" {
+let moment4 = test "moment4" {
+
+    test "vs_mathnet" {
+        let! xs = Gen.Double.[-1000.0, 1000.0].Array.[5, 500]
+        let expected = RunningStatistics()
+        let actual = Moment4Estimator()
+        for x in xs do
+            expected.Push x
+            actual.Add x
+        Check.close VeryHigh expected.Mean actual.Mean
+        Check.close VeryHigh expected.Variance actual.Variance
+        Check.close VeryHigh expected.StandardDeviation actual.StandardDeviation
+        Check.close VeryHigh expected.Skewness actual.Skewness
+        Check.close VeryHigh expected.Kurtosis actual.Kurtosis
+    }
 
     test "faster" {
         let! xs = Gen.Double.OneTwo.Array
@@ -107,16 +121,161 @@ let moment = test "moment" {
         for x in xs2 do e2.Add x
         let e3 = e1 + e2
         e1.Add e2
-        Check.equal e3.N e1.N |> Check.message "N"
-        Check.equal e3.M1 e1.M1 |> Check.message "M1"
-        Check.equal e3.S2 e1.S2 |> Check.message "M2"
-        Check.equal e3.S3 e1.S3 |> Check.message "M3"
-        Check.equal e3.S4 e1.S4 |> Check.message "M4"
+        Check.equal e3.N e1.N
+        Check.equal e3.M1 e1.M1
+        Check.equal e3.S2 e1.S2
+        Check.equal e3.S3 e1.S3
+        Check.equal e3.S4 e1.S4
+    }
+
+    test "add" {
+        let! xs1 = Gen.Double.OneTwo.Array.[5, 50]
+        and! xs2 = Gen.Double.OneTwo.Array.[5, 50]
+        let qe3 = Moment4Estimator()
+        let qe1 = Moment4Estimator()
+        for x in xs1 do
+            qe1.Add x
+            qe3.Add x
+        let qe2 = Moment4Estimator()
+        for x in xs2 do
+            qe2.Add x
+            qe3.Add x
+        let qe4 = qe1 + qe2
+        Check.close VeryHigh qe4.Mean qe3.Mean
+        Check.close VeryHigh qe4.Variance qe3.Variance
+        Check.close VeryHigh qe4.StandardDeviation qe3.StandardDeviation
+        Check.close VeryHigh qe4.Skewness qe3.Skewness
+        Check.close VeryHigh qe4.Kurtosis qe3.Kurtosis
+    }
+}
+
+let moment3 = test "moment3" {
+
+    test "vs_mathnet" {
+        let! xs = Gen.Double.[-1000.0, 1000.0].Array.[5, 500]
+        let expected = RunningStatistics()
+        let actual = Moment3Estimator()
+        for x in xs do
+            expected.Push x
+            actual.Add x
+        Check.close VeryHigh expected.Mean actual.Mean
+        Check.close VeryHigh expected.Variance actual.Variance
+        Check.close VeryHigh expected.StandardDeviation actual.StandardDeviation
+        Check.close VeryHigh expected.Skewness actual.Skewness
+    }
+
+    test "faster" {
+        let! xs = Gen.Double.OneTwo.Array
+        Check.faster
+            (fun () ->
+                let e = Moment3Estimator()
+                for x in xs do e.Add x
+            )
+            (fun () ->
+                let e = RunningStatistics()
+                for x in xs do e.Push x
+            )
+    }
+
+    test "add_same" {
+        let! xs1 = Gen.Double.OneTwo.Array.[5, 50]
+        and! xs2 = Gen.Double.OneTwo.Array.[5, 50]
+        let e1 = Moment3Estimator()
+        for x in xs1 do e1.Add x
+        let e2 = Moment3Estimator()
+        for x in xs2 do e2.Add x
+        let e3 = e1 + e2
+        e1.Add e2
+        Check.equal e3.N e1.N
+        Check.equal e3.M1 e1.M1
+        Check.equal e3.S2 e1.S2
+        Check.equal e3.S3 e1.S3
+    }
+
+    test "add" {
+        let! xs1 = Gen.Double.OneTwo.Array.[5, 50]
+        and! xs2 = Gen.Double.OneTwo.Array.[5, 50]
+        let qe3 = Moment3Estimator()
+        let qe1 = Moment3Estimator()
+        for x in xs1 do
+            qe1.Add x
+            qe3.Add x
+        let qe2 = Moment3Estimator()
+        for x in xs2 do
+            qe2.Add x
+            qe3.Add x
+        let qe4 = qe1 + qe2
+        Check.close VeryHigh qe4.Mean qe3.Mean
+        Check.close VeryHigh qe4.Variance qe3.Variance
+        Check.close VeryHigh qe4.StandardDeviation qe3.StandardDeviation
+        Check.close VeryHigh qe4.Skewness qe3.Skewness
+    }
+}
+
+let moment2 = test "moment2" {
+
+    test "vs_mathnet" {
+        let! xs = Gen.Double.[-1000.0, 1000.0].Array.[5, 500]
+        let expected = RunningStatistics()
+        let actual = Moment2Estimator()
+        for x in xs do
+            expected.Push x
+            actual.Add x
+        Check.close VeryHigh expected.Mean actual.Mean
+        Check.close VeryHigh expected.Variance actual.Variance
+        Check.close VeryHigh expected.StandardDeviation actual.StandardDeviation
+    }
+
+    test "faster" {
+        let! xs = Gen.Double.OneTwo.Array
+        Check.faster
+            (fun () ->
+                let e = Moment2Estimator()
+                for x in xs do e.Add x
+            )
+            (fun () ->
+                let e = RunningStatistics()
+                for x in xs do e.Push x
+            )
+    }
+
+    test "add_same" {
+        let! xs1 = Gen.Double.OneTwo.Array.[5, 50]
+        and! xs2 = Gen.Double.OneTwo.Array.[5, 50]
+        let e1 = Moment2Estimator()
+        for x in xs1 do e1.Add x
+        let e2 = Moment2Estimator()
+        for x in xs2 do e2.Add x
+        let e3 = e1 + e2
+        e1.Add e2
+        Check.equal e3.N e1.N
+        Check.equal e3.M1 e1.M1
+        Check.equal e3.S2 e1.S2
+    }
+
+    test "add" {
+        let! xs1 = Gen.Double.OneTwo.Array.[5, 50]
+        and! xs2 = Gen.Double.OneTwo.Array.[5, 50]
+        let qe3 = Moment2Estimator()
+        let qe1 = Moment2Estimator()
+        for x in xs1 do
+            qe1.Add x
+            qe3.Add x
+        let qe2 = Moment2Estimator()
+        for x in xs2 do
+            qe2.Add x
+            qe3.Add x
+        let qe4 = qe1 + qe2
+        Check.close VeryHigh qe4.Mean qe3.Mean
+        Check.close VeryHigh qe4.Variance qe3.Variance
+        Check.close VeryHigh qe4.StandardDeviation qe3.StandardDeviation
     }
 }
 
 let all =
     test "stats_estimator" {
         quartile
-        moment
+        moment4
+        moment3
+        moment2
     }
