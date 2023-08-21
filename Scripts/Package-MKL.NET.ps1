dcffa32a6f99196ae4849b059c56f6a5a2bb5994
Anthony Lloyd <anthony@lloydhq.com>
2023-08-21 12:22:16 +0100

    release MKL.NET 1.5.0

--- Scripts/Package-MKL.NET.ps1
+++ Scripts/Package-MKL.NET.ps1
@@ -3,7 +3,7 @@ dotnet build MKL.NET -c Release -r win-x86
 dotnet build MKL.NET -c Release -r linux-x64
 dotnet build MKL.NET -c Release -r osx-x64
 dotnet build MKL.NET -c Release -r osx-x86 # The ref assembly
-dotnet pack MKL.NET -c Release -o packages
+dotnet pack MKL.NET -c Release -o packages -p:NOCONT=true
 dotnet pack MKL.NET.Matrix -c Release -o packages -p:NORID=true
 dotnet pack MKL.NET.Optimization -c Release -o packages -p:NORID=true
 dotnet pack MKL.NET.Statistics -c Release -o packages -p:NORID=true
