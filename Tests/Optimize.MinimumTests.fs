201eabd4fb9ec4b638f61d9dd413ac1d6b703072
Anthony Lloyd <anthony@lloydhq.com>
2023-11-06 07:41:25 +0000

    CsCheck 3.0.0-rc4

--- Tests/Optimize.MinimumTests.fs
+++ Tests/Optimize.MinimumTests.fs
@@ -344,7 +344,7 @@ let all =
             let x = Array.create n -1.0
             let mutable count = 0
             let ols = Optimize.Minimum(1e-7, 0.0, Func<_,_>(fun x -> count <- count + 1; Optimization.Broyden x), x)
-            Check.between 164 164 count
+            Check.between 164 165 count
             Check.close High 0.0 ols
         }
 
